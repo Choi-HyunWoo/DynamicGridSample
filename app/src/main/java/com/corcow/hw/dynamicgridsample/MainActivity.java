f064508de4bfaaef078d77f0c3a7db207feb252e
Hyunwoo Choi <cho7673@gmail.com>
2016-04-01 18:56:02 +0900

    Initial commit

---
+++ app/src/main/java/com/corcow/hw/dynamicgridsample/MainActivity.java
@@ -0,0 +1,74 @@
+package com.corcow.hw.dynamicgridsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import org.askerov.dynamicgrid.DynamicGridView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    DynamicGridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (DynamicGridView) findViewById(R.id.dynamicGridView);
+        gridView.setAdapter(new MyDynamicGridAdapter(this,
+                new ArrayList<String>(Arrays.asList(Cheeses.sCheeseStrings)),
+                3));
+//        add callback to stop edit mode if needed
+//        gridView.setOnDropListener(new DynamicGridView.OnDropListener()
+//        {
+//            @Override
+//            public void onActionDrop()
+//            {
+//                gridView.stopEditMode();
+//            }
+//        });
+        gridView.setOnDragListener(new DynamicGridView.OnDragListener() {
+            @Override
+            public void onDragStarted(int position) {
+                Log.d(TAG, "drag started at position " + position);
+            }
+
+            @Override
+            public void onDragPositionsChanged(int oldPosition, int newPosition) {
+                Log.d(TAG, String.format("drag item position changed from %d to %d", oldPosition, newPosition));
+            }
+        });
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                gridView.startEditMode(position);
+                return true;
+            }
+        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, parent.getAdapter().getItem(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (gridView.isEditMode()) {
+            gridView.stopEditMode();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
